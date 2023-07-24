6b338dd36c9db9ef1c7a012f4883b4e03abee460
shkimhi <ajfqhksh1@gmail.com>
2023-07-25 00:03:01 +0900

    No2675

---
+++ src/five/No11654.java
@@ -0,0 +1,17 @@
+package five;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class No11654 {
+    public static void array() throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+
+        System.out.println((int)a);
+
+    }
+
+}
