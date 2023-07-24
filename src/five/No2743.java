366dfdf4ca6705a413df94105585539ea53989bf
shkimhi <ajfqhksh1@gmail.com>
2023-07-24 22:51:57 +0900

    No2743+

--- src/five/No2743.java
+++ src/five/No2743.java
@@ -1,4 +1,17 @@
 package five;
 
+import java.io.*;
+
 public class No2743 {
+    public static void String() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        bw.write(String.valueOf(str.length()));
+        bw.close();
+
+
+    }
+
 }
