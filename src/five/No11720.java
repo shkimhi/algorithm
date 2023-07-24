474ac0b1539783d4dc3482011982fcdaaa64c92c
shkimhi <ajfqhksh1@gmail.com>
2023-07-24 23:04:14 +0900

    No11720

--- src/five/No11720.java
+++ src/five/No11720.java
@@ -1,13 +1,8 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+package five;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        String();
-    }
+import java.io.*;
+
+public class No11720 {
     public static void String() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -22,4 +17,5 @@ public class Main {
 
         bw.close();
     }
+
 }
