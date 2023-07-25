26a04443beeda4c48cd60b54a8a423670649ff9a
shkimhi <ajfqhksh1@gmail.com>
2023-07-25 22:27:31 +0900

    No11718 (EOF 입력 법)

--- src/five/No11718.java
+++ src/five/No11718.java
@@ -1,4 +1,17 @@
 package five;
 
+import java.io.*;
+
 public class No11718 {
+    public static void String() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str;
+        while ((str = br.readLine()) != null){
+            System.out.println(str);
+        }
+
+    }
+
 }
