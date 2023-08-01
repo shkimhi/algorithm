038ed10195e5ff02c8b760d18cd0bbcfffdb08ae
shkimhi <ajfqhksh1@gmail.com>
2023-08-01 23:06:28 +0900

    No2839 설탕배달

--- src/etc/No2839.java
+++ src/etc/No2839.java
@@ -1,12 +1,9 @@
+package etc;
+
 import java.io.*;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        processInput();
-    }
-
+public class No2839 {
     public static void processInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -31,5 +28,5 @@ public class Main {
 
         bw.close();
     }
-}
 
+}
