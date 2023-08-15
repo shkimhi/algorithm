fd73537dfe48b2da411b67ba76fc138663d416da
shkimhi <ajfqhksh1@gmail.com>
2023-08-15 21:55:53 +0900

    출력

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,8 @@ public class Main {
     }
 
     public static void processInput() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            // 대문자면 소문자로 바꾸기
-            if (Character.isUpperCase(c)) {
-                sb.append(Character.toLowerCase(c));
-            } else {
-                // 소문자면 대문자로 바꾸기
-                sb.append(Character.toUpperCase(c));
-            }
-        }
-        System.out.print(sb);
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
 
     }
 }
