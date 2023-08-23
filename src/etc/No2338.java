47f55b8bc3b63b683477804aef0db2ab80eaa4db
shkimhi <ajfqhksh1@gmail.com>
2023-08-23 23:54:11 +0900

    No2338 BigInteger

--- src/etc/No2338.java
+++ src/etc/No2338.java
@@ -1,14 +1,9 @@
+package etc;
+
 import java.io.*;
 import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Stream;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        processInput();
-    }
-
+public class No2338 {
     public static void processInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -40,4 +35,5 @@ public class Main {
         br.close();
 
     }
+
 }
