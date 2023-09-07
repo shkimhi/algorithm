32ffbda092e69473c7c859e9bb228b12f1e2f86d
shkimhi <ajfqhksh1@gmail.com>
2023-09-07 22:27:28 +0900

    No1100

--- src/etc/No1100.java
+++ src/etc/No1100.java
@@ -1,14 +1,8 @@
+package etc;
+
 import java.io.*;
-import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Stream;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        processInput();
-    }
-
+public class No1100 {
     public static void processInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -35,7 +29,5 @@ public class Main {
         }
         bw.write(String.valueOf(answer));
         bw.close();
-
-
     }
 }
