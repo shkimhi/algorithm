a7b8f727282c23eca18c00bcee5c289135c944d7
shkimhi <ajfqhksh1@gmail.com>
2024-01-23 23:17:56 +0900

    No2750 BubbleSort 구현

--- src/etc/No2750.java
+++ src/etc/No2750.java
@@ -1,8 +1,30 @@
-import java.io.*;
-import java.util.*;
+package etc;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+import java.io.*;
+import java.util.Arrays;
+
+public class No2750 {
+
+    public void arr_sort() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i =0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int a : arr){
+            bw.write(String.valueOf(a)+"\n");
+        }
+        bw.close();
+        br.close();
+
+    }
+
+    public void bubbleSort() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
