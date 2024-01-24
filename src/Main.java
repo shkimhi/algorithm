ba6c2b69359968b72ca75d7c26220a3d987dec1e
shkimhi <ajfqhksh1@gmail.com>
2024-01-24 23:40:27 +0900

    +

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,9 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        int[] arr = new int[2];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
 
-        for(int a = 0; a <arr.length; a++) {
-            for (int i = 0, j = 1; j < arr.length; i++, j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
 
-        for(int a : arr){
-            bw.write(String.valueOf(a)+"\n");
-        }
-        bw.close();
-        br.close();
     }
 }
