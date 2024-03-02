06dfeb5e3a118933820e2e0565df08a196a135df
shkimhi <ajfqhksh1@gmail.com>
2024-03-02 19:19:39 +0900

    테스트코드

---
+++ src/etc/test2.java
@@ -0,0 +1,20 @@
+package etc;
+
+import java.io.*;
+
+public class test2 {
+    public static void processInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(a*b));
+
+
+
+        bw.close();
+    }
+
+}
