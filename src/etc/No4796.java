62cec56d33ef857e0375a8e2e0da778acaa26306
shkimhi <ajfqhksh1@gmail.com>
2024-04-01 14:23:24 +0900

    No4796 캠핑

---
+++ src/etc/No4796.java
@@ -0,0 +1,26 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No4796 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (l == 0 && p == 0 && v == 0) break;
+            int total = l * (v / p) + Math.min(l, v % p);
+            sb.append("Case " + i + ": " + total + "\n");
+            i++;
+        }
+        System.out.print(sb);
+    }
+}
