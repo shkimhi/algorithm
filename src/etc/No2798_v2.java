4385e3114e42956b9b079a90cf94ba23ade2c723
shkimhi <ajfqhksh1@gmail.com>
2024-04-07 23:59:06 +0900

    2798다른풀이

---
+++ src/etc/No2798_v2.java
@@ -0,0 +1,41 @@
+package etc;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No2798_v2 {
+    public static void processInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = 0;
+        int sum = 0;
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                for(int k=j+1; k<n; k++){
+                    sum = arr[i]+arr[j]+arr[k];
+                    if(sum == m ){
+                        result = sum;
+                        break;
+                    }
+                    if(result < sum && sum < m){
+                        result = sum;
+                    }
+
+                }
+            }
+        }
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+
+}
