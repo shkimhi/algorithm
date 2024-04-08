e2debc50af3957868f0407820ddc7f3d641f1457
shkimhi <ajfqhksh1@gmail.com>
2024-04-08 13:21:13 +0900

    20년3회문제

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int studnet = Integer.parseInt(br.readLine());
-        int[][] studentarr = new int[studnet][5];
 
-        for(int i=0; i<studnet; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<5; j++) {
-                studentarr[i][j] = Integer.parseInt(st.nextToken());
+        int i = 0;
+        int sum =0;
+        while (i<10){
+            i++;
+            if(i%2 == 1){
+                continue;
             }
+            sum += i;
         }
-        int max = -1;
-        int president = -1;
-
-        for(int i = 1; i<studnet; i++){
-            int cnt = 0;
-            for(int j = 0; j<studnet; j++){
-                if(i == j) continue;
-                for(int k =0; k<5; k++){
-                    if(studentarr[i][k] == studentarr[j][k]){
-                        cnt++;
-                        break;
-                    }
-                }
-            }
-            if(cnt>max){
-                max = cnt;
-                president = i+1;
-            }
-        }
-        System.out.println(president);
+        System.out.println(sum);
     }
 }
