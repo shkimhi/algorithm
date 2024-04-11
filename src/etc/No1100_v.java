3b464cf33e85662fbe15fc2726c6f8a08f23f2be
shkimhi <ajfqhksh1@gmail.com>
2024-04-11 23:23:44 +0900

    1100_v2

---
+++ src/etc/No1100_v.java
@@ -0,0 +1,33 @@
+package etc;
+
+import java.io.*;
+
+public class No1100_v {
+    public static void processInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str[] = new String[8];
+        int i = 0;
+        int answer = 0;
+        while (i<8){
+            str = br.readLine().split("");
+            if(i%2 == 0) {
+                for (int j = 0; j < 8; j += 2) {
+                    if (str[j].equals("F")) {
+                        answer++;
+                    }
+                }
+            }else {
+                for(int j = 1; j<8; j+=2){
+                    if(str[j].equals("F")){
+                        answer++;
+                    }
+                }
+            }
+            i++;
+        }
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+}
