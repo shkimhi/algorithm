88aa4a3d29e23e6f17b5a2310e1ccc36034a3eb2
shkimhi <ajfqhksh1@gmail.com>
2024-04-25 14:17:14 +0900

    23년 2회 1번문제

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,11 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());;
-        int n[] = new int[5];
-        int i;
 
-        for(i = 0;i<5; i++){
-            n[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(i=0; i<5; i++){
-            System.out.println(n[(i+1)%5]);
-        }
-
+    int c = 0;
+    for(int i = 1; i<=2023; i++){
+        if(i%4 == 0) c++;
+    }
+        System.out.println(c);
     }
 }
